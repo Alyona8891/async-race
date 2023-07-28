2624e6162b375172a0930f2efe72f36d3a24a195
unknown <besssta888@mail.ru>
2023-07-28 15:42:41 +0300

    refactor: refactor elementCreator.ts

--- async-race/src/components/units/elementCreator.ts
+++ async-race/src/components/units/elementCreator.ts
@@ -1,59 +1,70 @@
-import { CallbackObject, FunctionObject, ParametersElementCreator, ParametersInputCreator } from '../../types/types';
+import {
+  CallbackObject, FunctionObject, ParametersElementCreator, ParametersInputCreator,
+} from '../../types/types';
 
 export default class ElementCreator {
-    element: Element | null;
+  element: Element | null;
 
-    innerHTML: string | undefined;
+  innerHTML: string | undefined;
 
-    constructor(parameters: ParametersElementCreator) {
-        this.element = null;
-        this.createElement(parameters);
+  inputElement: HTMLInputElement | null;
+
+  inputColorElement: HTMLInputElement | null;
+
+  buttonElement: HTMLButtonElement | null;
+
+  constructor(parameters: ParametersElementCreator) {
+    this.element = null;
+    this.inputElement = null;
+    this.inputColorElement = null;
+    this.buttonElement = null;
+    this.createElement(parameters);
+  }
+
+  createElement(parameters: ParametersElementCreator | ParametersInputCreator): void {
+    this.element = document.createElement(parameters.tag);
+    this.setClasses(parameters.tagClasses);
+    this.setTextContent(parameters.textContent);
+    if (parameters.callback) {
+      this.setCallBack(parameters.callback);
     }
+  }
 
-    createElement(parameters: ParametersElementCreator | ParametersInputCreator): void {
-        this.element = document.createElement(parameters.tag);
-        this.setClasses(parameters.tagClasses);
-        this.setTextContent(parameters.textContent);
-        if (parameters.callback) {
-            this.setCallBack(parameters.callback);
-        }
+  getCreatedElement(): Element | HTMLElement | undefined {
+    if (this.element) {
+      return this.element;
     }
+    return undefined;
+  }
 
-    getCreatedElement(): Element | HTMLElement | undefined {
-        if (this.element) {
-            return this.element;
-        }
-        return undefined;
+  addInnerElement(element: Element | ElementCreator | undefined): void {
+    if (element instanceof ElementCreator) {
+      const createdElement = element.getCreatedElement();
+      if (createdElement) {
+        this.element?.append(createdElement);
+      }
+    } else if (element) {
+      this.element?.append(element);
     }
+  }
 
-    addInnerElement(element: Element | ElementCreator | undefined): void {
-        if (element instanceof ElementCreator) {
-            const createdElement = element.getCreatedElement();
-            if (createdElement) {
-                this.element?.append(createdElement);
-            }
-        } else if (element) {
-            this.element?.append(element);
-        }
+  setClasses(parameters: string[] | null): void {
+    if (parameters) {
+      parameters.forEach((className) => this.element?.classList.add(className));
     }
+  }
 
-    setClasses(parameters: string[] | null): void {
-        if (parameters) {
-            parameters.forEach((className) => this.element?.classList.add(className));
-        }
+  setTextContent(parameter: string): void {
+    if (this.element) {
+      this.element.textContent = parameter;
     }
+  }
 
-    setTextContent(parameter: string): void {
-        if (this.element) {
-            this.element.textContent = parameter;
-        }
+  setCallBack(callbackObject: CallbackObject | FunctionObject): void {
+    if (this.element) {
+      Object.entries(callbackObject).forEach(([key, value]) => {
+        this.element?.addEventListener(key, value);
+      });
     }
-
-    setCallBack(callbackObject: CallbackObject | FunctionObject): void {
-        if (this.element) {
-            Object.entries(callbackObject).forEach(([key, value]) => {
-                this.element?.addEventListener(key, value);
-            });
-        }
-    }
+  }
 }
--- async-race/src/components/units/inputCreator/inputCreator.ts
+++ async-race/src/components/units/inputCreator/inputCreator.ts
@@ -3,28 +3,22 @@ import { ParametersInputCreator } from '../../../types/types';
 import ElementCreator from '../elementCreator';
 
 export default class InputCreator extends ElementCreator {
-    inputElement!: HTMLInputElement;
-
-    inputColorElement!: HTMLInputElement;
-
-    buttonElement!: HTMLButtonElement;
-
-    createElement(parameters: ParametersInputCreator): void {
-        this.element = document.createElement('div');
-        this.setClasses(parameters.tagClasses);
-        if (parameters.callback) {
-            this.setCallBack(parameters.callback);
-        }
-        this.inputElement = document.createElement('input');
-        this.element.append(this.inputElement);
-        this.inputColorElement = document.createElement('input');
-        this.inputColorElement.setAttribute('type', 'color');
-        this.element.append(this.inputColorElement);
-        this.buttonElement = document.createElement('button');
-        this.buttonElement.classList.add('input-block__button');
-        if (parameters.buttonName) {
-            this.buttonElement.innerText = parameters.buttonName.toUpperCase();
-        }
-        this.element.append(this.buttonElement);
+  createElement(parameters: ParametersInputCreator): void {
+    this.element = document.createElement('div');
+    this.setClasses(parameters.tagClasses);
+    if (parameters.callback) {
+      this.setCallBack(parameters.callback);
     }
+    this.inputElement = document.createElement('input');
+    this.element.append(this.inputElement);
+    this.inputColorElement = document.createElement('input');
+    this.inputColorElement.setAttribute('type', 'color');
+    this.element.append(this.inputColorElement);
+    this.buttonElement = document.createElement('button');
+    this.buttonElement.classList.add('input-block__button');
+    if (parameters.buttonName) {
+      this.buttonElement.innerText = parameters.buttonName.toUpperCase();
+    }
+    this.element.append(this.buttonElement);
+  }
 }
