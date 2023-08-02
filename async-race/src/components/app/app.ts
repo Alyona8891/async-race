3ec4cf02b0fb19643b06c1408ed5ce469faca57a
unknown <besssta888@mail.ru>
2023-08-02 09:36:12 +0300

    fix: fix calling new App

--- async-race/src/components/app/app.ts
+++ async-race/src/components/app/app.ts
@@ -3,14 +3,9 @@ import HeaderView from './view/header/headerView';
 import MainView from './view/main/mainView';
 
 export default class App {
-  constructor() {
-    App.createView();
-  }
-
-  static createView(): void {
+  createView(): void {
     const mainView: MainView = new MainView();
     const headerView: HeaderView = new HeaderView(mainView);
-
     const footerView: FooterView = new FooterView();
     const createdHeaderView = headerView.getElementCreator();
     const createdMainView = mainView.getElementCreator();
